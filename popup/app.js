8edc7c003950a6157f02dbbdd1a35fb12dc116b8
bangnokia <bangnokia@gmail.com>
2017-10-07 00:22:54 +0700

    init project

---
+++ popup/app.js
@@ -0,0 +1,17 @@
+(function() {
+
+	var App = function() {
+
+		this.run = function() {
+			this.init();
+		};
+
+		this.init = function() {
+			
+		};
+	};
+
+	var app = new App();
+
+	app.run();
+})();
